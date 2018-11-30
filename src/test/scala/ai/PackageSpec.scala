2842de16ca082d4615989bd3ceb3448de9be4c5b
Raffaello Bertini <raffaello.bertini@thirdbridge.com>
2018-11-30 17:47:43 +0000

    fixes

--- src/test/scala/ai/PackageSpec.scala
+++ src/test/scala/ai/PackageSpec.scala
@@ -9,26 +9,21 @@ class PackageSpec extends FlatSpec with Matchers {
   "Tic Tac Toe game with minimax" should "be generate all states and win" in {
     val game = new BoardTicTacToe()
     minimax(game, true) should be(0)
-    println (s"total calls: $minimaxCounter")
   }
 
   "Tic Tac Toe game with minimax depth" should "be generate all states and win" in {
     val game = new BoardTicTacToe()
-    minimaxCounter=0
-    minimax(game, 0, true) should be(0)
-    println (s"total calls: $minimaxCounter")
+    minimax(game, true) should be(0)
   }
 
   "Tic Tac Toe negamax" should "solve the game" in {
     val game = new BoardTicTacToe()
     negamax(game, 1) should be(0)
-//    println (s"total calls: $negamaxCounter")
   }
 
   "Tic Tac Toe alphaBeta" should "solve the game" in {
     val game = new BoardTicTacToe()
     alphaBeta(game, maximizingPlayer = true) should be(0)
-//    println (s"total calls: $alphaBetaCounter")
   }
 
   "Player 1 Tic Tac Toe" should "win" in {
@@ -40,6 +35,7 @@ class PackageSpec extends FlatSpec with Matchers {
     game.board(2)(1) = 2
     game.board(2)(2) = 2
     negamax(game, 1) should be (1)
+    minimax(game, true) should be (1)
   }
 
   "Player 2 Tic Tac Toe" should "win" in {
@@ -50,7 +46,8 @@ class PackageSpec extends FlatSpec with Matchers {
     game.board(1)(1) = 1
     game.board(2)(1) = 2
     game.board(2)(2) = 2
-    game.board(2)(1) = 1
+    game.board(1)(2) = 1 // => @todo: fix this test
     negamax(game, -1) should be (-1)
+    minimax(game, false) should be (-1)
   }
 }
