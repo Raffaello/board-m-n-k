f137547278190793158c010db9ee1e11dec7d20c
Raffaello Bertini <raffaello.bertini@thirdbridge.com>
2018-12-21 15:01:24 +0000

    add BoardMNK(3, 3, 3) tests

--- src/test/scala/ai/AlphaBetaTranspositionSpec.scala
+++ src/test/scala/ai/AlphaBetaTranspositionSpec.scala
@@ -1,6 +1,6 @@
 package ai
 
-import game.BoardTicTacToe
+import game.{BoardMNK, BoardTicTacToe}
 import org.scalatest.{Matchers, WordSpec}
 
 class AlphaBetaTranspositionSpec extends WordSpec with Matchers {
@@ -14,7 +14,24 @@ class AlphaBetaTranspositionSpec extends WordSpec with Matchers {
       t.beta shouldEqual Int.MaxValue
       t.depth shouldEqual 0
     }
+    "have first move" in {
+      val t = game.nextMove(true, 0)
+      t.score shouldEqual 0
+      t.alpha shouldEqual 0
+      t.beta shouldEqual Int.MaxValue
+      t.depth shouldEqual 0
+    }
+  }
 
+  "BoardMNK 3x3x3 Alpha Beta with Transposition" should {
+    val game = new BoardMNK(3, 3, 3) with AlphaBetaTransposition
+    "solve the game" in {
+      val t = game.solve()
+      t.score shouldEqual 0
+      t.alpha shouldEqual 0
+      t.beta shouldEqual Int.MaxValue
+      t.depth shouldEqual 0
+    }
     "have first move" in {
       val t = game.nextMove(true, 0)
       t.score shouldEqual 0
