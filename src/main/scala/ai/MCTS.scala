d201b15573d655e2ad8a6061b6b55cfae678ead7
Raffaello Bertini <raffaellobertini@gmail.com>
2019-04-27 21:09:02 +0100

    MCTS - basic (#9)
    * init
    
    * clean up code (#10)
    
    * remove deprecated
    
    * remove unused code
    
    * code review
    
    * init
    
    * init

---
+++ src/main/scala/ai/MCTS.scala
@@ -0,0 +1,15 @@
+package ai
+
+import scala.collection.mutable
+
+trait MCTS extends AiBoard {
+
+
+  def selection() = ???
+
+  def expansion() = ???
+
+  def simulation() = ???
+
+  def backpropagation() = ???
+}
