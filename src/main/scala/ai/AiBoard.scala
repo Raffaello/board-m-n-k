ee9151c314fa84683fdfe399ee60a8b40036cdbf
Raffaello Bertini <raffaellobertini@gmail.com>
2019-05-30 00:16:22 +0100

    clean comments

--- src/main/scala/ai/AiBoard.scala
+++ src/main/scala/ai/AiBoard.scala
@@ -2,9 +2,6 @@ package ai
 
 import game.{BoardMNKP, Score, Status}
 
-/**
-  * Only for 2 player at the moment.
-  */
 trait AiBoard extends BoardMNKP with AiStats {
   final val aiPlayer: Byte = config.getInt("player").toByte
 
