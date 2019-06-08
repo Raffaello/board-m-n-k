c4197c9e8857d431143412fe17b8858a3cc7ab26
Raffaello Bertini <raffaellobertini@gmail.com>
2019-06-08 22:48:36 +0100

    fixes

--- src/main/scala/game/boards/lookups/MLookup.scala
+++ src/main/scala/game/boards/lookups/MLookup.scala
@@ -6,5 +6,5 @@ import scala.collection.immutable.NumericRange
 
 trait MLookup extends BoardMNType {
 
-  lazy val mLookups: NumericRange[Int] = NumericRange[Int](0, mn, m)
+  lazy val mLookups: NumericRange[Int] = NumericRange[Int](0, mn, n)
 }
