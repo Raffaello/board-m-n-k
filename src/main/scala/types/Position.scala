eb883d8a2bec1d5d88bbf76cbf291c54e09ce3d0
Raffaello Bertini <raffaellobertini@gmail.com>
2019-06-17 01:03:57 +0100

    Update Position.scala

--- src/main/scala/types/Position.scala
+++ src/main/scala/types/Position.scala
@@ -1,4 +1,20 @@
 package types
+/*
+sealed trait Position {
+  def row: Short
+  def col: Short
+}
+
+case object NilPosition extends Position {
+  override def row: Short = -1
+
+  override def col: Short = -1
+}
+
+final case class Position2(row: Short, col: Short) extends Position {
+  def min: Short = Math.min(row, col).toShort
+}
+ */
 
 final case class Position(row: Short, col: Short) {
   def min: Short = Math.min(row, col).toShort
